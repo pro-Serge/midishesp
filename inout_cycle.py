e963e0882fd18dc8ad1932bbf7f1bdbfbd24b82e
pro-Serge <119916157+pro-Serge@users.noreply.github.com>
2025-05-14 21:03:14 +0300

    Add files via upload

---
+++ inout_cycle.py
@@ -0,0 +1,33 @@
+# This Python file uses the following encoding: utf-8
+import serial, time
+
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+test_packet1 = ("55 0D 04 ff 00 00 00 00 00 00 00 00 00 00 00 00")
+packet1 = bytearray.fromhex(test_packet1)
+crc1 = sum(packet1[2:15])
+packet1[15] = crc1 & 0xFF;
+print("Отправил:",f"{packet1.hex()}")
+ser.write(bytes(packet1))
+
+time.sleep(0.1)  # Короткая пауза
+print("Ответ 1 :",ser.read_all().hex())
+
+time.sleep(0.1)  # Короткая пауза
+
+for b in range(256):
+ test_packet2 = ("55 0D 09 00 00 00 00 00 00 00 00 00 00 00 00 00")
+ packet2 = bytearray.fromhex(test_packet2)
+ packet2[3] = b
+ #packet2[14] = b
+ crc2 = sum(packet2[2:15])
+ packet2[15] = crc2 & 0xFF;
+ print("Отправил:",f"{packet2.hex()}")
+ ser.write(bytes(packet2))
+ time.sleep(0.1)  # Короткая пауза
+ print("Ответ 1 :",ser.read_all().hex())
+
+
+time.sleep(2.1)  # Длинная пауза
+print("Ответ 2 :",ser.read_all().hex())
+
